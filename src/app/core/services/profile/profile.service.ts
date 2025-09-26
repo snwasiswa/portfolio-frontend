364dad7ddff914bc0ea9415b6326b11200b76678
snwasiswa <stevewasiswa@gmail.com>
2025-09-25 23:39:36 -0400

    Fixing production bugs

--- src/app/core/services/profile/profile.service.ts
+++ src/app/core/services/profile/profile.service.ts
@@ -1,29 +1,41 @@
 import { Injectable } from '@angular/core';
 import { Profile } from '../../models/profile/profile.model';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { environment} from '../../../../environments/environment';
-
+import { Observable, from } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  private apiUrl = '';
+  private resumeUrl = '';
 
-  private apiUrl = environment.apiUrl + 'api/profiles/';
-  private readonly resumeUrl = environment.apiUrl + 'api/download-resume/';
-
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    // Load API URLs dynamically from config.json
+    this.http.get<{ apiUrl: string }>('/config.json').subscribe(config => {
+      this.apiUrl = config.apiUrl + 'api/profiles/';
+      this.resumeUrl = config.apiUrl + 'api/download-resume/';
+    });
+  }
 
   getProfile(): Observable<Profile> {
     return this.http.get<Profile>(this.apiUrl);
   }
 
   downloadResumeWithPassword(password: string): Observable<Blob> {
-  return this.http.post(this.resumeUrl,
-    { password },
-    { responseType: 'blob' }
-  );
+    // Ensure resumeUrl is loaded before sending request
+    return from(new Promise<void>((resolve) => {
+      if (this.resumeUrl) {
+        resolve();
+      } else {
+        this.http.get<{ apiUrl: string }>('/config.json').subscribe(config => {
+          this.resumeUrl = config.apiUrl + 'api/download-resume/';
+          resolve();
+        });
+      }
+    })).pipe(
+      switchMap(() => this.http.post(this.resumeUrl, { password }, { responseType: 'blob' }))
+    );
+  }
 }
-}
-
--- src/app/core/services/projects/projects.service.ts
+++ src/app/core/services/projects/projects.service.ts
@@ -1,21 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, from } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { Project } from '../../models/projects/project.model';
-import {environment} from '../../../../environments/environment';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectsService {
+  private apiUrl = '';
 
-  private apiUrl = environment.apiUrl + '/api/projects/';
-
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    // Load API URL dynamically from config.json
+    this.http.get<{ apiUrl: string }>('/config.json').subscribe(config => {
+      this.apiUrl = config.apiUrl + 'api/projects/';
+    });
+  }
 
   // This function returns an Observable of Project array
   getProjects(): Observable<Project[]> {
-    return this.http.get<Project[]>(this.apiUrl);
+    // Ensure apiUrl is loaded before sending request
+    return from(new Promise<void>((resolve) => {
+      if (this.apiUrl) {
+        resolve();
+      } else {
+        this.http.get<{ apiUrl: string }>('/config.json').subscribe(config => {
+          this.apiUrl = config.apiUrl + 'api/projects/';
+          resolve();
+        });
+      }
+    })).pipe(
+      switchMap(() => this.http.get<Project[]>(this.apiUrl))
+    );
   }
 }
